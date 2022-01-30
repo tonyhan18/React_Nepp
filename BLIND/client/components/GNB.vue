a00985d2d353ac8921e213e56ac286e9f6255237
TonyHan <ourclub7279@gmail.com>
2022-01-30 16:05:25 +0900

    tmp

--- BLIND/client/components/GNB.vue
+++ BLIND/client/components/GNB.vue
@@ -1,22 +1,32 @@
 <template>
-  <nav>
-    <div class="side-block">
-      <nuxt-link to="/">
-        <img src="/logo/main.jpg" alt="알럼나이 로고" />
-      </nuxt-link>
-      <nuxt-link to="/"> 홈 </nuxt-link>
-      <nuxt-link to="/company"> 기업리뷰 </nuxt-link>
-    </div>
-    <div class="side-block">
-      <SmallSearchbar />
-      <a @click.prevent="clickWritingButton"> 글쓰기 </a>
-      <a @click.prevent="clickLoginButton">
-        {{ user.email ? "로그아웃" : "로그인" }}
-      </a>
-    </div>
-    <LoginModal />
-    <WritingModal />
-  </nav>
+  <div class="nav-container">
+    <nav>
+      <div class="side-block">
+        <nuxt-link to="/" id="logo-btn">
+          <img src="/logo/main.png" alt="블라인드 로고" />
+        </nuxt-link>
+        <nuxt-link
+          to="/"
+          :class="['text-menu', $route.name === 'index' && 'active']"
+          >홈</nuxt-link
+        >
+        <nuxt-link
+          to="/company"
+          :class="['text-menu', $route.name === 'company' && 'active']"
+          >기업 리뷰</nuxt-link
+        >
+      </div>
+      <div class="side-block">
+        <SmallSearchbar />
+        <a @click.prevent="clickWritingButton" id="write-btn">글쓰기</a>
+        <a @click.prevent="clickLoginButton" id="login-btn">{{
+          user.email ? "로그아웃" : "로그인"
+        }}</a>
+      </div>
+      <LoginModal />
+      <WritingModal />
+    </nav>
+  </div>
 </template>
 <script>
 import SmallSearchbar from "@/components/GNB/SmallSearchbar";
@@ -36,25 +46,76 @@ export default {
     clickWritingButton() {
       if (!this.user.email) {
         this.$store.commit("modal/SET_LOGIN_MODAL_OPEN");
+        return;
       }
+      this.$store.commit("modal/SET_WRITING_MODAL_STATE", true);
     },
     clickLoginButton() {
       if (!this.user.email) {
         this.$store.commit("modal/SET_LOGIN_MODAL_OPEN");
+        return;
       }
+      this.logout();
+    },
+    logout() {
+      this.$store.commit("user/SET_USER", {
+        email: null,
+        nickname: null,
+      });
     },
   },
 };
 </script>
 <style lang="scss" scoped>
+.nav-container {
+  border-bottom: 1px solid #d4d4d4;
+}
 nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
   width: 100%;
+  height: 80px;
+  margin: auto;
+  padding: 0 20px;
+  max-width: 1100px;
   .side-block {
     display: flex;
-    align-items: flex-end;
+    height: 100%;
+    align-items: center;
+    #logo-btn {
+      margin-right: 60px;
+    }
+    .text-menu {
+      color: #222;
+      font-size: 20px;
+      margin-right: 30px;
+    }
+    #write-btn {
+      background: rgb(218, 50, 56);
+      color: white;
+      font-size: 14px;
+      height: 40px;
+      margin-left: 10px;
+      width: 82px;
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      font-weight: 700;
+    }
+    #login-btn {
+      background: white;
+      color: rgb(34, 34, 34);
+      font-size: 14px;
+      border: solid 1px rgb(212, 212, 212);
+      height: 40px;
+      margin-left: 10px;
+      width: 82px;
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      font-weight: 700;
+    }
   }
 }
 </style>
