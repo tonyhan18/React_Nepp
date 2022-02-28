7d9c470940d32513c16673898b8f9b66b0cbb0d0
tonyhan18 <ourclub7279@gmail.com>
2022-02-28 13:51:47 +0900

    0228 PRE update

---
+++ 0.PRE/insta/src/apis/user.js
@@ -0,0 +1,20 @@
+import { Instance } from "./index";
+
+export const addUser = async (params) => {
+  try {
+    const result = await Instance.post("/users", params);
+    return result.data;
+  } catch ({ response }) {
+    return response.data;
+  }
+};
+
+export const getToken = async (params) => {
+  console.log(params);
+  try {
+    const result = await Instance.post("/users/token", params);
+    return result.data;
+  } catch ({ response }) {
+    return response.data;
+  }
+};
